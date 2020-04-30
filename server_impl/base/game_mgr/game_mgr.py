c0278d5eef9c2d6a6f3ff5981e6c68f1294e44d1
Q1143316492 <1143316492@qq.com>
2020-04-30 14:32:12 +0800

    备份

--- server_impl/base/game_mgr/game_mgr.py
+++ server_impl/base/game_mgr/game_mgr.py
@@ -3,6 +3,9 @@ from server_core import config
 from server_core.memcache import MemCache as Cache
 from server_core.event_controller import EventController, DelayEvent
 from server_impl.server_config import ckv
+from server_impl.base.sync_mgr import frame_sync
+from server_impl.base.room_mgr import game_room, room_mgr
+from server_impl.base.user_mgr import user_mgr
 
 
 def delay_stop_match(user_id, matching_time):
@@ -58,4 +61,24 @@ def matching_some_players(controller, user_list, room_id):
         num += 1
         if num > 20:
             break
-    return num, 0, ""
+    sync_controller = frame_sync.FrameSync(room_id)
+    controller.mem_cache.set(ckv.get_ckv_action_list(room_id), sync_controller)
+    return num, 0, ""  # num, ret, err_msg
+
+
+def init_room_runtime(controller, room_id):
+    room_runtime_key = ckv.get_ckv_room_runtime(room_id)
+    room_runtime = controller.mem_cache.get(room_runtime_key)
+    if room_runtime is None:
+        room_runtime = room_mgr.GameRoom(room_id)
+        controller.mem_cache.set(room_runtime_key, room_runtime)
+    return room_runtime
+
+
+def init_user_runtime(controller, user_id):
+    user_runtime_key = ckv.get_ckv_user_runtime(user_id)
+    user_runtime = controller.mem_cache.get(user_runtime_key)
+    if user_runtime is None:
+        user_runtime = user_mgr.UserRuntime(user_id)
+        controller.mem_cache.set(user_runtime_key, user_runtime)
+    return user_runtime
